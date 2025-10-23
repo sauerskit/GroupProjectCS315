74f586499946b7225639be56fd1398d0c9c5b823
penguinzarecool <151074650+penguinzarecool@users.noreply.github.com>
2025-10-23 01:57:25 -0700

    Finished RLE Compression
    Sill no decompression

--- rle.c
+++ rle.c
@@ -13,11 +13,45 @@ size_t RLE( const unsigned char *input,
 
     printf( "RLE\n\n" );
 
-    size_t size = 0;
+    symbol_t symbol;
+    int size = 0;
+    size_t i = 0;
 
-    for( size_t i = 0; i < len; i++ ) {
-        //output[i] = (symbol_t)input[i];
+    while( i < len ) {
+
+        int zeroRun = -1;
+        symbol = ( symbol_t )input[i];
+        output[size] = symbol;
+        size++;
+        printf( "element: %d\n", input[i] );
+
+        do {
+            zeroRun += ( input[i] == 0 ) ? 1 : 0;
+        } while( input[++i] == 0 && i < len );
+        
+        if( zeroRun > -1 ) {
+            int mod = zeroRun % 2;
+            printf( "zeroRun: %d| ",  zeroRun );
+            printf( "mod: %d\n", mod );
+            output[size] = ( mod ) ? RUNB : RUNA;
+            size++;
+            while( zeroRun > 1 ) {
+                zeroRun = zeroRun / 2;
+                mod = zeroRun % 2;
+                printf( "zeroRun: %d| ",  zeroRun );
+                printf( "mod: %d\n", mod );
+                output[size] = ( mod ) ? RUNB : RUNA;
+                size++;
+            }
+        }
     }
+
+    printf( "\n" );
+    for( i = 0; i < size; i++ ) {
+        printf( "%d ", output[i] );
+    }
+    
+    printf( "\n\n" );
 
     return size;
 }
